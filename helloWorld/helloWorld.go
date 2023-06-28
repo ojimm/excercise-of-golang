3a33eeef7c1fb6f68364d02cdd0d741f6e61d56d
unknown <wangzmg@gmail.com>
2023-06-28 12:29:24 +0800

    initial

---
+++ helloWorld/helloWorld.go
@@ -0,0 +1,9 @@
+// 要运行这个程序，先将将代码放到名为 hello-world.go 的文件中，然后执行 go run
+// 如果我们想将程序编译成二进制文件（Windows 平台是 .exe 可执行文件）， 可以通过 go build 来达到目的。
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
